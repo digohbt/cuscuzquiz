03c74142f2cbdaf15f5ea8711b7938a5ea1f818d
Diego Teodoro <digohbt@gmail.com>
2021-01-25 23:21:17 -0300

    aula-inicial-05

--- src/components/Head/index.js
+++ src/components/Head/index.js
@@ -6,10 +6,10 @@ function IndexPage() {
       <Head>
         <title>My page title</title>
         <meta property="og:title" content="CuscuzQuiz" key="title" />
+        <meta property="og:image" content="blob:https://vercel.com/9e49b3b3-3821-4178-89a3-e040157271dd"/>
       </Head>
-      <Head>
-      <meta property="og:image" content="blob:https://vercel.com/9e49b3b3-3821-4178-89a3-e040157271dd"/>      </Head>
-      <p>Hello world!</p>
+      
+
     </div>
   )
 }
